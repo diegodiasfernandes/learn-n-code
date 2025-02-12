6d2fea375a703619c69e1e8065b3c06ebb517612
Diego Dias <diegodiasfernandes.comp@gmail.com>
2025-02-12 14:59:08 -0300

    new test settup

--- max3sat_aco/main.py
+++ max3sat_aco/main.py
@@ -2,10 +2,10 @@ from ACO import ACO
 from graphFunc.graph_util import plot_line_graph_polynomial_fit
 from randomSearch import RandomSearch
 
-path_to_sat = 'max3sat_aco\examples\SAT1.txt'
+path_to_sat = 'max3sat_aco\examples\SAT3.txt'
 
-num_tests = 10
-avg_performance = 0
+num_tests = 300
+'''avg_performance = 0
 avg_time = 0
 best_random_perf = 0
 rand_search = RandomSearch(path_to_sat)
@@ -19,7 +19,7 @@ for _ in range(num_tests):
 
 print(f'Avg RandomSearch performance: {avg_performance / num_tests}')
 print(f'Highest performance: {best_random_perf}')
-print(f'Finished in {avg_time / num_tests}s')
+print(f'Finished in {avg_time / num_tests}s')'''
 
 avg_performance = 0
 avg_time = 0
@@ -32,13 +32,22 @@ for _ in range(num_tests):
     avg_time += time
     if best_aco_perf < best_ant.performance:
         best_aco_perf = best_ant.performance
+        best_solution = best_ant.parameters
+        best_aco = aco
 
 print(f'Avg ACO performance: {avg_performance / num_tests}')
-print(f'Highest performance: {best_aco_perf}')
+print(f'Highest performance: {best_aco_perf} {best_aco_perf / aco.total_clauses_num}')
+print(f'Best performance: {best_solution}')
 print(f'Finished in {avg_time / num_tests}s')
 
 # Plotting graph
-plot_line_graph_polynomial_fit([ant.performance for ant in aco.ants_arr], x_ticks=list(range(1, len(aco.ants_arr), 10)))
+plot_line_graph_polynomial_fit(
+    [ant.performance for ant in best_aco.ants_arr], 
+    x_ticks=list(range(1, len(best_aco.ants_arr), 10)),
+    graph_title='Performance de Cada Formiga através das Iterações',
+    x_label='Iteração',
+    y_label='Performance'
+)
 
 """
 Best results so far:
